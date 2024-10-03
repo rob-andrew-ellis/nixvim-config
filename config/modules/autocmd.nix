b410d216af8a5d3b080cd6e58e7731812984c3c6
rob-andrew-ellis <robandrewellis@gmail.com>
2024-10-03 21:57:29 +0100

    Added options

---
+++ config/modules/autocmd.nix
@@ -0,0 +1,29 @@
+{
+  autoCmd = [
+    { # Highlight text when yanking
+      event = ["TextYankPost"];
+      desc = "Highlight when yanking text";
+      group = "highlight-yank";
+      callback.__raw = ''
+        function()
+          vim.highlight.on_yank()
+        end
+      '';
+    }
+
+    {
+      event = ["VimEnter"];
+      desc = "Wrap code blocks in elipses";
+      callback.__raw = ''
+        function()
+          vim.cmd([[
+            vim.augroup MarkdownSyntaxMatch
+              autocmd!
+              autocmd FileType markdown syntax match @conceal /```/ conceal cchar=…
+            augroup END
+          ]])
+        end
+      '';
+    }
+  ];
+}
--- config/modules/options/init.nix
+++ config/modules/options/init.nix
@@ -1,8 +1,36 @@
 {
   opts = {
+    # Line numbers
     number = true;
     relativenumber = true;
-    conceallevel = 2;
-    mouse = "a";
+
+    # General
+    conceallevel = 2;  # Hide certain characters to make markdown more readable
+    list = true;  # Sets how certain whitespace characters are displayed
+    listchars.__raw = "{ tab = '  ', trail = '·', nbsp = '␣' }";  # Raw lua
+    mouse = "a";  # Allow mouse mode
+    showmode = false;  # Disable mode visual in favour of bar
+    signcolumn = "yes";  # Dets the sign column to be always on
+    undofile = true;  # Saves undo history
+    timeoutlen = 300;  # Decreases mapped sequence wait time
+    updatetime = 250;  # Decrease update time
+    incommand = "split";  # Preview substitutions
+    cursorline = true;  # Highlight cursor line
+    scrolloff = 10;  # Minimum number of lines above or below the cursor before scrolling
+    breakindent = true;  # Wraps lines on spaces
+    spelllang = "en_gb";  # Sets spelling language
+    ignorecase = true;  # Case sensitivity in search
+    smartcase = true;  # ^
+    splitright = true;  # Defaults vertical splits to the right
+    splitbelow = true;  # Defaults horizontal splits below
+
+    # Sync clipboard with OS 
+    clipboard = {
+      providers = {
+        wl-copy = true;
+      };
+
+      register = "unnamedplus";
+    };
   };
 }
